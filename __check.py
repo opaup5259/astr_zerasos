929603bdda6d696fcde2ec6df604d0fd47d77548
opaup5259 <opaup5259@gmail.com>
2026-07-13 02:40:28 +0800

    buf fixed

--- __check.py
+++
@@ -1,8 +0,0 @@
-import os, subprocess
-os.chdir(r'D:\_workspace\astr_zerasos')
-r = subprocess.run(['git', 'log', '--oneline', '-3'], capture_output=True, text=True)
-print(r.stdout.strip())
-r2 = subprocess.run(['git', 'status', '--short'], capture_output=True, text=True)
-print(r2.stdout.strip() or 'clean')
-r3 = subprocess.run(['git', 'rev-parse', 'HEAD'], capture_output=True, text=True)
-print('HEAD:', r3.stdout.strip())
